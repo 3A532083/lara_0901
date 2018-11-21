ad37caf7af3ae9f15c4f2a85a365320c0fd64b22
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-11-21 12:17:35 +0800

    練習4~5

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -28,4 +28,11 @@ class AdminPostsController extends Controller
 
         return view('admin.posts.edit', $data);
     }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+        return redirect()->route('admin.posts.index');
+    }
+
 }
